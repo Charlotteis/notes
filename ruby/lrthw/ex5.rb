cb13e9e570b42ff36a857a1e8b4c6392704a09ce
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-09-18 13:36:31 +0100

    feat: exercises one to ten of LRTHW

---
+++ ruby/lrthw/ex5.rb
@@ -0,0 +1,21 @@
+name = 'Charlotte Spencer'
+age = 25 # as of 2016
+height = 63 # inches
+weight = 140 # pounds
+eyes = 'Blue and Green'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "They are #{height} inches tall."
+puts "They are #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "They have #{eyes} and #{hair} hair."
+puts "Their teeth are usually #{teeth} depending on the coffee."
+
+# This line is tricky, try to get it exactly right.
+puts "If I add #{age}, #{height}, and #{weight} I get: " \
+     "#{age + height + weight}."
+
+puts "Their height in centimetres is: #{height * 2.54}cm"
+puts "Their weight in kilograms is: #{weight * 0.453592}cm"
