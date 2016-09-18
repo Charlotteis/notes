1f2583119e5e449481da4a4c42897a82555e9ac0
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-09-18 15:50:39 +0100

    feat: exercises 11 to 20 or LRTHW

---
+++ ruby/lrthw/ex11.rb
@@ -0,0 +1,15 @@
+print 'How old are you? '
+age = gets.chomp
+print 'How tall are you? '
+height = gets.chomp
+print 'How much do you weigh? '
+weight = gets.chomp
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
+print 'Now, what\'s your favourite colour? '
+colour = gets.chomp
+print 'And your favourite number? '
+number = gets.chomp
+
+puts "Your favourite colour is #{colour} and your fave number is #{number}? Cool!"
